1c9a2a19f4a7f7e3932ef37e74c24d49bef43bb6
Silasak Lawong <silasak.lf@gmail.com>
2016-02-11 13:00:25 +0700

    upload
    xx

---
+++ index.php
@@ -0,0 +1,212 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Mercury</title>
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/flexslider.css" rel="stylesheet">
+    <link  href="css/animate.min.css" rel="stylesheet">
+
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<header>
+    <div class="head-nav">
+        <div class="container">
+            <div class="logo visible-xs"><a href=""><img src="images/logo.png" alt=""></a></div>
+            <div class="menu-mobile" onclick="$('.nav-item').slideToggle('fast')"><i class="fa fa-bars"></i></div>
+            <div class="text-center">
+                <ul class="list-unstyled nav-item">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Products</a></li>
+                    <li><a href="">About Us</a></li>
+                    <li class="hidden-xs"><a href=""><img src="images/logo.png" alt=""></a></li>
+                    <li><a href="">Service</a></li>
+                    <li><a href="">Career</a></li>
+                    <li><a href="">Contact Us</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="banner text-center">
+    <img class="img-responsive" style="display: inline-block" src="images/banner.jpg" alt="">
+</section>
+<section class="section section-about">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+                <h1>เกี่ยวกับเรา</h1>
+                <div class="detail">
+                    ร้าน Mercury Car Detailing เป็นศูนย์ ล้าง ขัดเคลือบสี เคลือบคริสตัล ที่ให้บริการมาตราฐานระดับพรีเมี่ยม
+                    ที่ดีที่สุดในย่านรามคำแหง เพื่อท่านเจ้าของรถที่หลงไหลความเงางาม ความสะอาด และรายละเอียดในการดูแล
+                    รถยนต์ พร้อมการปกป้องที่ดีที่สุดสำหรับรถที่ท่านรัก ด้วยการบริการระดับมืออาชีพที่มีประสบการ์ณ์<br><br>
+                    และได้รับการยอมรับจากลูกค้าหลายพันคัน น้ำยาและเครื่องมือที่ทันสมัยที่สุด เพื่อความสมบูรณ์แบบใน
+                    การดูแลรถยนต์ของท่าน <span><a href="">More >></a></span>
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <h1>Mercury Car Detailing  </h1>
+                <div class="detail">
+                    ล้างทำความสะอาดภายนอก ภายใน Premium Wash & Vacuum
+                    บริการล้างรถมาตราฐานระดับพรีเมี่ยม ด้วยผลิตภัณฑ์คุณภาพสูง
+                    และเครื่องมือที่ทันสมัย อุปกรณ์ต่าง ๆ ที่ใช้ในการล้างทำความสะอาดรถ
+                    เป็นสิ่งที่ Mercury Car Detailing ให้ความสำคัญเป็นอย่างมาก
+                    รวมถึงทักษะและขั้นตอนการล้างอย่างละเอียด
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+</section>
+
+<section class="banner-slide">
+    <!-- Place somewhere in the <body> of your page -->
+    <div class="flexslider">
+        <ul class="slides">
+            <li>
+                <img src="images/banner-slide1.jpg" />
+            </li>
+            <li>
+                <img src="images/banner-slide2.jpg" />
+            </li>
+            <li>
+                <img src="images/banner-slide1.jpg" />
+            </li>
+            <li>
+                <img src="images/banner-slide2.jpg" />
+            </li>
+
+        </ul>
+    </div>
+</section>
+<section class="section-service about-item">
+    <div class="container">
+        <h1>บริการของเรา</h1>
+        <div class="row">
+            <div class="col-sm-6">
+                <ul class="list-unstyled" >
+                    <li>
+                        <div class="row">
+                            <div class="col-sm-6 text-center wow fadeInLeft" >
+                                <img class="img-responsive" src="images/list1.png" alt="">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="title">เคลือบ Crystal & Ceramic  </div>
+                                <div class="detail">โดดเด่นด้วยผลิตภัณฑ์นำเข้าแบรนด์ชั้นนำ จากต่างประเทศทั้งหมด</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="row">
+                            <div class="col-sm-6 text-center wow fadeInLeft" data-wow-delay="0.2s">
+                                <img class="img-responsive" src="images/list2.png" alt="">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="title">เคลือบ Crystal & Ceramic  </div>
+                                <div class="detail">โดดเด่นด้วยผลิตภัณฑ์นำเข้าแบรนด์ชั้นนำ จากต่างประเทศทั้งหมด</div>
+                            </div>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="row">
+                            <div class="col-sm-6 text-center wow fadeInLeft" data-wow-delay="0.4s">
+                                <img class="img-responsive" src="images/list3.png" alt="">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="title">เคลือบ Crystal & Ceramic  </div>
+                                <div class="detail">โดดเด่นด้วยผลิตภัณฑ์นำเข้าแบรนด์ชั้นนำ จากต่างประเทศทั้งหมด</div>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-sm-6">
+                <div class="list4">
+                    <img class="img-responsive wow fadeIn" src="images/show-car.jpg" alt="">
+                    <div class="title">
+                        เคลือบแก้ว | Glass Coating
+                    </div>
+                    <div class="detail ">
+                        การเคลือบแก้วของเราโดดเด่นด้วยผลิตภัณฑ์นำเข้าแบรนด์ชั้นนำจากต่างประเทศทั้งหมด รวมถึงทักษะ
+                        และประสบการณ์ในการเตรียมพื้นผิวและเคลือบ Crystal & Ceramicมากว่า 2,000คัน และความเข้า
+                        ใจในกระบวนการต่างๆที่ได้รับการฝึกฝนและอบรมอย่างดี โดยกระบวนการทำเคลือบแก้วของเราจะ
+                        แตกต่าง ในทั้งกระบวนการขัดลบรอยบนผิวสี ไปจนถึงน้ำยาเคลือบแก้วสูตรเฉพาะ และการดูแล
+                        Maintenance ที่แตกต่างอย่างสิ้นเชิงกับแบรนด์ทั่วๆไปตามท้องตลาด ซึ่งมอบความเงาใสแบบสูงสุด
+                        ให้กับยานพาหนะของคุณ
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="footer">
+    <div class="container">
+        <div class="col-sm-2 ft-item"><a href="">Home</a></div>
+        <div class="col-sm-2 ft-item"><a href="">About Us</a></div>
+        <div class="col-sm-2 ft-item">
+            <a href="">Services</a>
+            <ul class="list-unstyled">
+                <li><a href=""> เคลือบ Crystal & Ceramic</a></li>
+                <li><a href=""> เคลือบสีรถยนต์</a></li>
+                <li><a href=""> ล้างรถมาตตรฐาน</a></li>
+            </ul>
+        </div>
+        <div class="col-sm-2 ft-item"><a href="">Promotion</a></div>
+        <div class="col-sm-2 ft-item"><a href="">Contact Us</a></div>
+        <div class="col-sm-2 text-center">
+            <ul class="list-unstyled sc-footer">
+                <li>
+                    <a href=""><i class="fa fa-facebook"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa fa-twitter"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa fa-youtube-play"></i></a>
+                </li>
+                <li>
+                    <a href=""><i class="fa fa-instagram"></i></a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</section>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+<script src="js/jquery.flexslider-min.js"></script>
+<script src="js/wow.min.js"></script>
+<!-- Place in the <head>, after the three links -->
+<script type="text/javascript" charset="utf-8">
+    new WOW().init();
+    $(window).load(function() {
+
+        $('.flexslider').flexslider({
+            animation: "slide",
+
+        });
+    });
+</script>
+
+</body>
+</html>
